cd20167061fd4159d7b81e7be6073129b8557c57
Osmair Agi <osmair.coelho@gmail.com>
2025-08-07 09:12:55 -0300

    Calcule o quadrado de um numero

---
+++ src/Ex10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//Calcule o quadrado de um numero
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println("Quadrado: " + (n * n));
+
+    }
+}
